8fc1761cac84ace2244193de50fd57d9e909daa8
daank <81301584+daank9@users.noreply.github.com>
2024-07-11 21:11:09 +0200

    Add files via upload

--- ButtonManager.cs
+++ ButtonManager.cs
@@ -15,17 +15,19 @@ public class ButtonManager : MonoBehaviour
     {
         
     }
-
+    //Begin met het spelen van de game
     public void startGame()
     {
         SceneManager.LoadScene("SampleScene");  
     }
 
+    //Stop met het spelen van de game
     public void ExitGame()
     {
         Application.Quit();
     }
 
+    //Ga terug naar het start menu van de game
     public void MenuGame()
     {
         SceneManager.LoadScene("Menu");
