bcbe48b7cda1df5c268c21b709aed7c4bd938875
daank <81301584+daank9@users.noreply.github.com>
2024-07-12 08:14:21 +0200

    Added sound effect code to platform script

--- Platform.cs
+++ Platform.cs
@@ -4,18 +4,27 @@ using UnityEngine;
 
 public class Platform : MonoBehaviour
 {
+
+    // Hier wordt de plaftorm bounce sterkte bepaald
     public float jumpForce = 10f;
+    [SerializeField] private AudioSource audioSource;
+    [SerializeField] private AudioClip audioClip;
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.relativeVelocity.y <= 0f)
         {
+            
             Rigidbody2D rb = collision.gameObject.GetComponent<Rigidbody2D>();
             if(rb != null)
             {
+                
                 Vector2 velocity = rb.velocity;
                 velocity.y = jumpForce;
                 rb.velocity = velocity;
+                audioSource.clip = audioClip;
+                audioSource.Play();
+                
             }
         }
     }
